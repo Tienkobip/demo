c6361715d2d0a284c23e2844a634c7ad48059bd1
Tienkobip <jackacevn@gmail.com>
2024-06-23 18:20:43 +0700

    Sua file test.cs

--- test/test.cs
+++ test/test.cs
@@ -5,5 +5,7 @@ class Program1
     static void Main()
     {
         Console.WriteLine("Hello world");
+        Console.WriteLine("Chao Cac ban");
+        Console.WriteLine("Chao may con bitches, toi t3 nay choi bida nha");
     }
 }
